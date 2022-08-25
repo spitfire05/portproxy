f059949c71a2e05d5ae68cb48d7a2c1594415257
Michal Borejszo <michael.borejszo@gmail.com>
2022-08-25 17:35:12 +0200

    move config to separate mod

---
+++ src/config.rs
@@ -0,0 +1,42 @@
+use std::{env, fs};
+
+use color_eyre::eyre::{eyre, Result, WrapErr};
+use derive_getters::Getters;
+use serde::Deserialize;
+
+#[derive(Deserialize, Getters)]
+pub struct Config {
+    proxy: Option<Vec<Proxy>>,
+}
+
+#[derive(Deserialize, Getters)]
+pub struct Proxy {
+    listen: String,
+    connect: String,
+}
+
+pub fn get_config_path() -> Option<String> {
+    if let Ok(path) = env::var("PORTPROXY_CONFIG") {
+        // Use $PORTPROXY_CONFIG as the config path if available
+        log::debug!("PORTPROXY_CONFIG is set: {}", &path);
+
+        Some(path)
+    } else {
+        // Default to using ~/.config/portproxy.toml
+        log::debug!("PORTPROXY_CONFIG is not set");
+        let config_path = dirs::home_dir()?.join(".config/portproxy.toml");
+        let config_path_str = config_path.to_str()?.to_owned();
+        log::debug!("Using default config path: {}", config_path_str);
+
+        Some(config_path_str)
+    }
+}
+
+pub fn load() -> Result<Config> {
+    let path = get_config_path().ok_or(eyre!("Failed to get config path"))?;
+    let cfg_bytes = fs::read(&path).wrap_err_with(|| eyre!("Cannot open file \"{}\"", path))?;
+    let cfg: Config =
+        toml::from_slice(&cfg_bytes).wrap_err_with(|| eyre!("Cannot parse config"))?;
+
+    Ok(cfg)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
+mod config;
 mod proxy;
 
-use std::fs;
 use std::net::SocketAddr;
 
-use color_eyre::eyre::{bail, eyre, Context, Result};
+use color_eyre::eyre::{bail, eyre, Result, WrapErr};
 use env_logger::Env;
 use futures::future::join_all;
 use proxy::TcpProxy;
-use serde::Deserialize;
 use tokio::net::lookup_host;
-
-#[derive(Deserialize)]
-struct Config {
-    proxy: Option<Vec<Proxy>>,
-}
-
-#[derive(Deserialize)]
-struct Proxy {
-    listen: String,
-    connect: String,
-}
 
 async fn str_to_sock_addr(input: &str) -> Result<SocketAddr> {
     let results = lookup_host(input)
@@ -39,19 +27,16 @@ async fn main() -> Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     color_eyre::install()?;
 
-    let cfg_bytes =
-        fs::read("config.toml").wrap_err_with(|| eyre!("Cannot open file \"config.toml\""))?;
-    let cfg: Config =
-        toml::from_slice(&cfg_bytes).wrap_err_with(|| eyre!("Cannot parse config"))?;
+    let cfg = config::load()?;
 
     let mut proxies = vec![];
 
-    match cfg.proxy {
+    match cfg.proxy() {
         None => bail!("No proxies defined in config"),
         Some(proxy_list) => {
-            for p in &proxy_list {
-                let listen = str_to_sock_addr(&p.listen).await?;
-                let connect = str_to_sock_addr(&p.connect).await?;
+            for p in proxy_list {
+                let listen = str_to_sock_addr(p.listen()).await?;
+                let connect = str_to_sock_addr(p.connect()).await?;
 
                 let proxy = TcpProxy::new(listen, connect);
                 proxies.push(proxy);
