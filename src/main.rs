ec299ab38ea2333350dd170e8e60e8c066f4a245
Michal Borejszo <Michal.Borejszo@Emerson.com>
2023-03-20 11:31:04 +0100

    Improve error msges with miette

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod config;
 mod proxy;
 
-use color_eyre::eyre::{bail, Result};
 use env_logger::Env;
 use futures::future::join_all;
+use miette::{bail, Result};
 use proxy::TcpProxy;
 use tokio::net::lookup_host;
 
@@ -12,7 +12,6 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::Builder::from_env(Env::default().filter_or("PORTPROXY_LOG", "info")).init();
-    color_eyre::install()?;
 
     log::info!("portproxy v{} starting...", VERSION);
 
@@ -40,7 +39,7 @@ async fn main() -> Result<()> {
         }
     }
 
-    join_all(proxies.iter().map(|p| p.run())).await;
+    join_all(proxies.iter().map(proxy::TcpProxy::run)).await;
 
     log::info!("Nothing left to do, exiting..");
 
